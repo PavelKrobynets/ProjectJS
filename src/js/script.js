30f3188440bddc6ba03e4fb5caac7e43e4879289
Pavel <krobynetspavel@gmail.com>
2024-03-16 14:13:52 +0200

    ended first lesson

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,20 @@
 "use strict";
 
-let isChecked = true,
-  isClosed = false;
-console.log(isChecked || isClosed);
-console.log(isChecked && isClosed);
+let numberOfFilms = +prompt("How  many films you watched ?", "");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Last movie you saw?", ""),
+	b = prompt("Rate movie from 1 to 10", ""),
+	c = prompt("Last movie you saw?", ""),
+	d = prompt("Rate movie from 1 to 10", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
