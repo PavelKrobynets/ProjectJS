ca1483cbfa988e84e857fc1b93418decdc9f020b
Pavel <krobynetspavel@gmail.com>
2024-03-17 15:43:48 +0200

    inside loops

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,32 @@
 "use strict";
 
-let num = 50;
-
-while (num <= 55) {
-  console.log(num);
-  num++;
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+  for (let j = 0; j < 3; j++) {
+    console.log(`j = ${j}`);
+  }
 }
 
-do {
-  console.log(num);
-  num++;
-} while (num <= 55);
+let res = "";
+const length = 7;
 
-for (let i = 1; i < 8; i++){
-	console.log(i);
+for (let i = 1; i < length; i++) {
+  for (let k = 0; k < i; k++) {
+    res += "*";
+		console.log(k);
+  }
+  res += "\n";
+}
+
+console.log(res);
+
+first: for (let i = 0; i < 3; i++) {
+  console.log(`First level ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`second level = ${j}`);
+    for (let k = 0; k < 3; k++) {
+			if (k === 2) continue first; // skip the rest of this loop and go to  next iteration of outer loop
+      console.log(`third level = ${k}`);
+    }
+  }
 }
