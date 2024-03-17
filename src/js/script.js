8119e671c78fc4892f2c2d8d7d7854e2ed533dac
Pavel <krobynetspavel@gmail.com>
2024-03-17 15:05:29 +0200

    secound lesson

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,17 @@
 "use strict";
 
-let numberOfFilms = +prompt("How  many films you watched ?", "");
+let num = 50;
 
-let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
+while (num <= 55) {
+  console.log(num);
+  num++;
+}
 
-const a = prompt("Last movie you saw?", ""),
-	b = prompt("Rate movie from 1 to 10", ""),
-	c = prompt("Last movie you saw?", ""),
-	d = prompt("Rate movie from 1 to 10", "");
+do {
+  console.log(num);
+  num++;
+} while (num <= 55);
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+for (let i = 1; i < 8; i++){
+	console.log(i);
+}
