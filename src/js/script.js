585215856b57dc6e7327182a5bf76a7f2b07c781
Pavel <krobynetspavel@gmail.com>
2024-03-17 16:03:20 +0200

    movies question loop

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,39 @@
-"use strict";
+'use strict';
 
-for (let i = 0; i < 3; i++) {
-  console.log(i);
-  for (let j = 0; j < 3; j++) {
-    console.log(`j = ${j}`);
-  }
-}
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let res = "";
-const length = 7;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-for (let i = 1; i < length; i++) {
-  for (let k = 0; k < i; k++) {
-    res += "*";
-		console.log(k);
-  }
-  res += "\n";
-}
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-console.log(res);
-
-first: for (let i = 0; i < 3; i++) {
-  console.log(`First level ${i}`);
-  for (let j = 0; j < 3; j++) {
-    console.log(`second level = ${j}`);
-    for (let k = 0; k < 3; k++) {
-			if (k === 2) continue first; // skip the rest of this loop and go to  next iteration of outer loop
-      console.log(`third level = ${k}`);
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
     }
-  }
+		if(i  === 1){
+			alert("Thank you for your answers!!");
+		}
 }
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
