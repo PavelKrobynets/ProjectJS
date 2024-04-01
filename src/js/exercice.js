2299ad1230ca24c40f6aa22f19cb25e9a2d86432
Pavel <krobynetspavel@gmail.com>
2024-04-01 19:44:38 +0300

    more recurtion functions

--- src/js/exercice.js
+++ src/js/exercice.js
@@ -421,9 +421,109 @@
 // console.log(document.querySelector('[data-current="5"]').nextSibling);
 // console.log(document.querySelector('[data-current="5"]').previousSibling);
 
-for (let node  of document.body.childNodes) {
-	if(node.nodeName == "#text"){
-		continue;
-	}
-	console.log(node);
+// for (let node  of document.body.childNodes) {
+// 	if(node.nodeName == "#text"){
+// 		continue;
+// 	}
+// 	console.log(node);
+// }
+
+// function pow(x, n) {
+//   let res = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     res *= x;
+//   }
+//   return res;
+// }
+
+// function powR(x, n) {
+//   if (n === 1) {
+//     return x;
+//   } else {
+//     return x * powR(x, n - 1);
+//   }
+// }
+// powR(2, 12);
+// console.log(powR(2, 8));
+
+let students = {
+  js: [
+    {
+      name: "Jhon",
+      progress: 100,
+    },
+    {
+      name: "Iban",
+      process: 60,
+    },
+  ],
+  html: {
+    basic: [
+      {
+        name: "Peter",
+        progress: 20,
+      },
+      {
+        name: "Ann",
+        progress: 18,
+      },
+    ],
+    pro: [
+      {
+        name: "Sam",
+        progress: 10,
+      },
+    ],
+  },
+};
+
+function getAverageProgress(data) {
+  let total = 0;
+  let students = 0;
+
+  for (let course of Object.values(data)) {
+    if (Array.isArray(course)) {
+      students += course.length;
+
+      for (let i = 0; i < course.length; i++) {
+        total += course[i].progress;
+      }
+    } else {
+      for (let subCourse of Object.values(course)) {
+        students += subCourse.length;
+
+        for (let i = 0; i < subCourse.length; i++) {
+          total += subCourse[i].progress;
+        }
+      }
+    }
+  }
+
+  return total / students;
 }
+
+console.log(getAverageProgress(students));
+
+function getAverageProgressByRecurtion(data) {
+  if (Array.isArray(data)) {
+    let total = 0;
+
+    for (let i = 0; i < data.length; i++) {
+      total += data[i].progress;
+    }
+    return [total, data.length];
+  } else {
+		let total = [0,0];
+		for (let subData of Object.values(data)) {
+			const subDataArr = getAverageProgressByRecurtion(subData);
+			total[0] += subDataArr[0];
+			total[1] += subDataArr[1];
+		}
+		return total;
+  }
+}
+
+const  result = getAverageProgressByRecurtion(students);
+console.log(`The average progress is ${result[0]} out of ${result[1]}`);
+console.log(result);
