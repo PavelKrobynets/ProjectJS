cd57943c2589c8439a9fd5d2945258199616116a
Pavel <krobynetspavel@gmail.com>
2024-04-03 20:12:29 +0300

    trying too solve sort problem

--- src/js/practice.js
+++ src/js/practice.js
@@ -1,41 +1,68 @@
 "use strict";
 
+document.addEventListener("DOMContentLoaded", () => {
+  const movieDB = {
+    movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против...",
+    ],
+  };
 
+  document.querySelectorAll(".promo__adv img").forEach((item) => {
+    item.remove();
+  });
 
-const movieDB = {
-  movies: [
-    "Логан",
-    "Лига справедливости",
-    "Ла-ла лэнд",
-    "Одержимость",
-    "Скотт Пилигрим против...",
-  ],
-};
+  const div = document.createElement("div"),
+    poster = document.querySelector(".promo__bg"),
+    genre = poster.querySelector(".promo__genre");
 
-document.querySelectorAll(".promo__adv img").forEach((item) => {
-  item.remove();
-});
-const div = document.createElement("div"),
-  poster = document.querySelector(".promo__bg"),
-  genre = poster.querySelector(".promo__genre"),
-  sortedMovies = movieDB.movies.sort(),
-  ul = document.querySelector(".promo__interactive-list"),
-  filmName = ul.querySelectorAll(".promo__interactive-item");
+  const ul = document.querySelector(".promo__interactive-list"),
+    filmName = ul.querySelectorAll(".promo__interactive-item");
 
-genre.textContent = "Драма";
-poster.style.backgroundImage = `url("img/bg.jpg")`;
+  const form = document.querySelector(".add"),
+    input = form.querySelector(".adding__input"),
+    checkBox = form.querySelector("[type='checkbox']");
 
-let li = document.createElement("li");
-li.classList.add("promo__interactive-item");
-for (let i = 0; i < sortedMovies.length; i++) {
-  li = `${i + 1} ${sortedMovies[i]}\n`;
-  filmName[i].textContent = li;
-}
+  genre.textContent = "Драма";
+  poster.style.backgroundImage = `url("img/bg.jpg")`;
 
-movieDB.movies.forEach((film, i) => {
-  ul.innerHTML = +`
-	<li class="promo__interactive-item">${i + 1} ${movieDB.movies[i]}
-	<div class="delete"></div>
-	</li>
-	`;
+  function movieList() {
+    let li = document.createElement("li");
+    movieDB.movies.sort();
+    for (let i = 0; i < movieDB.movies.length; i++) {
+      li = `${i + 1} ${movieDB.movies[i]}\n`;
+      filmName[i].textContent = li;
+    }
+  }
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newFilm = input.value,
+      li = document.createElement("li");
+
+    if (newFilm === "") {
+      alert("Введите название фильма!");
+      return false;
+    }
+
+    movieDB.movies.push(newFilm);
+    li.appendChild(document.createTextNode(`${newFilm}`));
+    ul.appendChild(li);
+    input.value = "";
+    movieList();
+  });
+
+  movieList();
+
+  // ul.appendChild(li);
+  // movieDB.movies.forEach((film, i) => {
+  //   ul.innerHTML = +`
+  // 	<li class="promo__interactive-item">${i + 1} ${movieDB.movies[i]}
+  // 	<div class="delete"></div>
+  // 	</li>
+  // 	`;
+  // });
 });
