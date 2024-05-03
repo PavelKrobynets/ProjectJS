79e2ec0687262e7931ee4227c75469b3edc1bd6e
Pavel <krobynetspavel@gmail.com>
2024-05-03 13:02:18 +0300

    how to calculate the speed of work

--- src/js/practice.js
+++ src/js/practice.js
@@ -164,3 +164,14 @@ console.log(now);
 
 now.setHours(18);
 console.log(now);
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++){
+	let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`${end - start} miliseconds`);
+console.log(end);
