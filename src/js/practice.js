c9c4544356eceeebdbe1e01325412692b5e98481
Pavel <krobynetspavel@gmail.com>
2024-05-09 15:47:37 +0300

    classes

--- src/js/practice.js
+++ src/js/practice.js
@@ -1,284 +1,284 @@
 "use strict";
+/*
+document.addEventListener("DOMContentLoaded", () => {
+  const movieDB = {
+    movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против...",
+    ],
+  };
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const movieDB = {
-//     movies: [
-//       "Логан",
-//       "Лига справедливости",
-//       "Ла-ла лэнд",
-//       "Одержимость",
-//       "Скотт Пилигрим против...",
-//     ],
-//   };
+  const deleteAdv = (arr) => {
+    arr.forEach((item) => {
+      item.remove();
+    });
+  };
 
-//   const deleteAdv = (arr) => {
-//     arr.forEach((item) => {
-//       item.remove();
-//     });
-//   };
+  const div = document.createElement("div"),
+    poster = document.querySelector(".promo__bg"),
+    genre = poster.querySelector(".promo__genre");
 
-//   const div = document.createElement("div"),
-//     poster = document.querySelector(".promo__bg"),
-//     genre = poster.querySelector(".promo__genre");
+  const ul = document.querySelector(".promo__interactive-list"),
+    filmName = ul.querySelectorAll(".promo__interactive-item");
 
-//   const ul = document.querySelector(".promo__interactive-list"),
-//     filmName = ul.querySelectorAll(".promo__interactive-item");
+  const form = document.querySelector(".add"),
+    input = form.querySelector(".adding__input"),
+    checkBox = form.querySelector("[type='checkbox']");
 
-//   const form = document.querySelector(".add"),
-//     input = form.querySelector(".adding__input"),
-//     checkBox = form.querySelector("[type='checkbox']");
+  const makeChanges = () => {
+    genre.textContent = "Драма";
+    poster.style.backgroundImage = `url("img/bg.jpg")`;
+  };
 
-//   const makeChanges = () => {
-//     genre.textContent = "Драма";
-//     poster.style.backgroundImage = `url("img/bg.jpg")`;
-//   };
+  const sortArr = (arr) => {
+    arr.sort();
+  };
 
-//   const sortArr = (arr) => {
-//     arr.sort();
-//   };
+  function createMovieList(films, parent) {
+    parent.innerHTML = "";
+    sortArr(films);
 
-//   function createMovieList(films, parent) {
-//     parent.innerHTML = "";
-//     sortArr(films);
+    movieDB.movies.forEach((film, i) => {
+      parent.innerHTML += `
+			<li class="promo__interactive-item">${i + 1} ${film}
+			<div class="delete"></div>
+			</li>
+			`;
+    });
 
-//     movieDB.movies.forEach((film, i) => {
-//       parent.innerHTML += `
-// 			<li class="promo__interactive-item">${i + 1} ${film}
-// 			<div class="delete"></div>
-// 			</li>
-// 			`;
-//     });
+    document.querySelectorAll(".delete").forEach((el, i) => {
+      el.addEventListener("click", () => {
+        el.parentElement.remove();
+        movieDB.movies.splice(i, 1);
+        createMovieList(films, parent);
+      });
+    });
+  }
 
-//     document.querySelectorAll(".delete").forEach((el, i) => {
-//       el.addEventListener("click", () => {
-//         el.parentElement.remove();
-//         movieDB.movies.splice(i, 1);
-//         createMovieList(films, parent);
-//       });
-//     });
-//   }
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-//   form.addEventListener("submit", (event) => {
-//     event.preventDefault();
+    let newFilm = input.value,
+      favorite = checkBox.checked;
 
-//     let newFilm = input.value,
-//       favorite = checkBox.checked;
+    if (newFilm) {
+      if (newFilm.length > 21) {
+        newFilm = `${newFilm.substring(0, 22)}...`;
+      }
+      favorite ? console.log("Adding favorite movie") : null;
+      movieDB.movies.push(newFilm);
+      createMovieList(movieDB.movies, ul);
+    } else {
+      alert("Edit mobie name");
+    }
+    event.target.reset();
+  });
 
-//     if (newFilm) {
-//       if (newFilm.length > 21) {
-//         newFilm = `${newFilm.substring(0, 22)}...`;
-//       }
-//       favorite ? console.log("Adding favorite movie") : null;
-//       movieDB.movies.push(newFilm);
-//       createMovieList(movieDB.movies, ul);
-//     } else {
-//       alert("Edit mobie name");
-//     }
-//     event.target.reset();
-//   });
+  deleteAdv(document.querySelectorAll(".promo__adv img"));
+  makeChanges();
+  createMovieList(movieDB.movies, ul);
+});
 
-//   deleteAdv(document.querySelectorAll(".promo__adv img"));
-//   makeChanges();
-//   createMovieList(movieDB.movies, ul);
-// });
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!! wrong way
+function movieList() {
+	movieDB.movies.sort();
+	movieDB.movies.forEach((film, i) => {
+		filmName[i].textContent = `${i + 1} ${movieDB.movies[i]}\n`;
+	});
+}
 
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!! wrong way
-// function movieList() {
-// 	movieDB.movies.sort();
-// 	movieDB.movies.forEach((film, i) => {
-// 		filmName[i].textContent = `${i + 1} ${movieDB.movies[i]}\n`;
-// 	});
-// }
+form.addEventListener("submit", (event) => {
+	const li = document.createElement("li"),
+		newMovie = input.value;
+	event.preventDefault();
+	if (newMovie === "") {
+		alert("Введите название фильма!");
+		return false;
+	}
 
-// form.addEventListener("submit", (event) => {
-// 	const li = document.createElement("li"),
-// 		newMovie = input.value;
-// 	event.preventDefault();
-// 	if (newMovie === "") {
-// 		alert("Введите название фильма!");
-// 		return false;
-// 	}
+	movieDB.movies.push(newMovie);
+	movieDB.movies.sort();
+	li.appendChild(document.createTextNode(newMovie));
+	li.classList.add("promo__interactive-item");
+	ul.appendChild(li);
+	input.value = "";
+	console.log(filmName);
+});
 
-// 	movieDB.movies.push(newMovie);
-// 	movieDB.movies.sort();
-// 	li.appendChild(document.createTextNode(newMovie));
-// 	li.classList.add("promo__interactive-item");
-// 	ul.appendChild(li);
-// 	input.value = "";
-// 	console.log(filmName);
-// });
+let user = { name: "Jhon" },
+  map = new WeakMap();
+map.set(user, "data");
 
-// let user = { name: "Jhon" },
-//   map = new WeakMap();
-// map.set(user, "data");
+user = null;
+console.log(map.has());
 
-// user = null;
-// console.log(map.has());
+let cache = new WeakMap();
 
-// let cache = new WeakMap();
+function cacheUser(user) {
+  if (!cache.has(user)) cache.set(user, Date.now());
+  return cache.get(user);
+}
 
-// function cacheUser(user) {
-//   if (!cache.has(user)) cache.set(user, Date.now());
-//   return cache.get(user);
-// }
+let lena = { name: "Elena" },
+  alex = { name: "Alex" };
 
-// let lena = { name: "Elena" },
-//   alex = { name: "Alex" };
+cacheUser(lena);
+cacheUser(alex);
 
-// cacheUser(lena);
-// cacheUser(alex);
+lena = null;
+console.log(cache.has(lena));
+console.log(cache.has(alex));
 
-// lena = null;
-// console.log(cache.has(lena));
-// console.log(cache.has(alex));
+let messages = [
+  { text: "Hello", from: "Jhon" },
+  { text: "World", from: "Helen" },
+  { text: "!!", from: "Amanda" },
+  { text: "...", from: "Michel" },
+];
 
-// let messages = [
-//   { text: "Hello", from: "Jhon" },
-//   { text: "World", from: "Helen" },
-//   { text: "!!", from: "Amanda" },
-//   { text: "...", from: "Michel" },
-// ];
+let readMessages = new WeakSet();
 
-// let readMessages = new WeakSet();
+readMessages.add(messages[0]);
+readMessages.add(messages[1]);
 
-// readMessages.add(messages[0]);
-// readMessages.add(messages[1]);
+const now1 = new Date(2020, 5, 1, 20);
+console.log(now1);
 
-// const now1 = new Date(2020, 5, 1, 20);
-// console.log(now1);
+const now2 = new Date(0);
+console.log(now2);
 
-// const now2 = new Date(0);
-// console.log(now2);
+const now = new Date();
+console.log(now);
 
-// const now = new Date();
-// console.log(now);
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getDay());
 
-// console.log(now.getFullYear());
-// console.log(now.getMonth());
-// console.log(now.getDate());
-// console.log(now.getHours());
-// console.log(now.getDay());
+console.log(now.getTime());
+console.log(now.getTimezoneOffset());
 
-// console.log(now.getTime());
-// console.log(now.getTimezoneOffset());
+now.setHours(18);
+console.log(now);
 
-// now.setHours(18);
-// console.log(now);
+let start = new Date();
 
-// let start = new Date();
+for (let i = 0; i < 100000; i++){
+	let some = i ** 3;
+}
 
-// for (let i = 0; i < 100000; i++){
-// 	let some = i ** 3;
-// }
+let end = new Date();
 
-// let end = new Date();
+alert(`${end - start} miliseconds`);
+console.log(end);
 
-// alert(`${end - start} miliseconds`);
-// console.log(end);
+const num = new Number(3);
+console.log(num);
 
-// const num = new Number(3);
-// console.log(num);
+function User(name, id) {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+  this.hello = function () {
+    console.log(`Hello ${this.name}`);
+  };
+}
+class User {
+	constructor(name, id){
+    this.name = name;
+    this.id = id;
+    this.human = true;
+  }
+  hello() {
+    console.log(`Hello ${this.name}`);
+  }
+	exit(){
+		console.log(`User ${this.name} left`);
+	}
+}
 
-// function User(name, id) {
-//   this.name = name;
-//   this.id = id;
-//   this.human = true;
-//   this.hello = function () {
-//     console.log(`Hello ${this.name}`);
-//   };
-// }
-// class User {
-// 	constructor(name, id){
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//   }
-//   hello() {
-//     console.log(`Hello ${this.name}`);
-//   }
-// 	exit(){
-// 		console.log(`User ${this.name} left`);
-// 	}
-// }
+User.prototype.exit = function (name) {
+  console.log(`User ${this.name} left`);
+};
 
-// User.prototype.exit = function (name) {
-//   console.log(`User ${this.name} left`);
-// };
+const ivan = new User("Ivan", 28);
+const alex = new User("Alex", 20);
 
-// const ivan = new User("Ivan", 28);
-// const alex = new User("Alex", 20);
+console.log(ivan);
+console.log(alex);
+ivan.hello();
 
-// console.log(ivan);
-// console.log(alex);
-// ivan.hello();
+function showThis(a, b) {
+  console.log(this);
+  function sum() {
+    console.log(this);
+    return a + b;
+  }
+  console.log(sum());
+}
+showThis(5, 2);
 
-// function showThis(a, b) {
-//   console.log(this);
-//   function sum() {
-//     console.log(this);
-//     return a + b;
-//   }
-//   console.log(sum());
-// }
-// showThis(5, 2);
+const obj = {
+	a: 20,
+	b: 15,
+	sum: function(){
+		console.log(this);
+	}
+};
+obj.sum();
 
-// const obj = {
-// 	a: 20,
-// 	b: 15,
-// 	sum: function(){
-// 		console.log(this);
-// 	}
-// };
-// obj.sum();
+class User {
+	constructor(name, id){
+    this.name = name;
+    this.id = id;
+    this.human = true;
+  }
+  hello() {
+    console.log(`Hello ${this.name}`);
+  }
+	exit(){
+		console.log(`User ${this.name} left`);
+	}
 
-// class User {
-// 	constructor(name, id){
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//   }
-//   hello() {
-//     console.log(`Hello ${this.name}`);
-//   }
-// 	exit(){
-// 		console.log(`User ${this.name} left`);
-// 	}
+class User1 {
+  name = String;
+  id = Number;
+  human = Boolean;
+}
 
-// class User1 {
-//   name = String;
-//   id = Number;
-//   human = Boolean;
-// }
+let igor = new User1();
+igor.name = "Igor";
+igor.id = 47;
+igor.human = true;
 
-// let igor = new User1();
-// igor.name = "Igor";
-// igor.id = 47;
-// igor.human = true;
+console.log(igor);
 
-// console.log(igor);
+function User(name, id) {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+	this.hello = function(){
+		console.log(`Hello i'm ${this.name}`);
+	}
+}
 
-// function User(name, id) {
-//   this.name = name;
-//   this.id = id;
-//   this.human = true;
-// 	this.hello = function(){
-// 		console.log(`Hello i'm ${this.name}`);
-// 	}
-// }
+let ivan = new User("Ivan", 23);
 
-// let ivan = new User("Ivan", 23);
+function sayName(){
+	console.log(this);
+	console.log(this.name);
+}
 
-// function sayName(){
-// 	console.log(this);
-// 	console.log(this.name);
-// }
+const user = {
+	name: 'John'
+}
 
-// const user = {
-// 	name: 'John'
-// }
-
-// sayName.call(user);
-// sayName.apply(user);
+sayName.call(user);
+sayName.apply(user);
 
 const btn = document.querySelector(".btn1");
 
@@ -299,3 +299,35 @@ const obj = {
 obj.sayNumber();
 
 const double = (a) => a * 2 ;
+
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  calcArea() {
+    return this.width * this.height;
+  }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+	constructor(height, width, text, bgColor){
+		super(height, width);
+		this.text = text;
+		this.bgColor = bgColor;
+	}
+	showMyProps(){
+		console.log(`Text ${this.text}, color ${this.bgColor}`);
+	}
+}
+
+const rect = new ColoredRectangleWithText(10, 10, "learning about classes", "red")
+rect.showMyProps();
+console.log(rect.calcArea());
+const square = new Rectangle(15, 15);
+const long = new Rectangle(20, 100);
+console.log(square.calcArea());
+console.log(long.calcArea());
+*/
